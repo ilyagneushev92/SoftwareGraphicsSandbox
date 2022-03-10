10843d2eb0ca4378a940146d34a90df917680ab7
ilyagneushev92 <ilya.gneushev92@gmail.com>
2022-03-10 17:47:20 +0300

    Bresenham Continue

--- SoftwareGraphicsSandbox/SoftwareGraphicsSandbox/Program.cs
+++ SoftwareGraphicsSandbox/SoftwareGraphicsSandbox/Program.cs
@@ -24,49 +24,101 @@ namespace SoftwareGraphicsSandbox {
             Renderer renderer = new Renderer(form.Handle);
             image = renderer.BackBuffer;
 
-            static void BresenhamLineOneQuadrant(int x0, int y0, int x1, int y1, Color32 color)
+            void BresenhamLineOneQuadrant(int x0, int x1, int y0 , int y1, Color32 color)
             {
-                int k;
-                
-                int _x0 = x0;
+
+
+
+                //var exchangeXY = (Math.Abs(y1 - y0) > Math.Abs(x1 - x0));
+
+
+                //if (exchangeXY)
+                //{
+                //    int e1 = x0;
+                //    x0 = y0;
+                //    y0 = e1;
+                //    int e2 = x1;
+                //    x1 = y1;
+                //    y1 = e2;
+                //}
+
+                int _x0 =  x0;
                 int _y0 = y0;
                 int _x1 = x1;
                 int _y1 = y1;
 
+                int signX = _x0 < _x1 ? 1 : -1;
+                int signY = _y0 < _y1 ? -1 : 1;
+                
+
+                float dx = _x1 - _x0;
+                float dy = _y1 - _y0;
+
+                float k = Math.Abs(dy / dx);
+            
+                float error = 1;
+
+
+                if (dx > dy) { 
                 int y = _y0;
-
-                
-
-                // reversed angle coefficient
-                k = (x1 - x0) / (y1 - y0);
-                
-                int error = 0;
-
-
-                for (int x = x0; x <= x1; x++)
-                {
-                    image.SetPixel(x, y, color);
-                    error++;
-                    if (error % k == 0) y--;
-
+                for (int x = _x0; x <= x1;) {
+                        image.SetPixel(x, y, color);
+                        error += k;
+                        x++;
+                        if (error >= 1)
+                    {
+                        
+                        y -= 1;
+                        error -= 1;
+                    }
+                    }
                 }
+
+                if (dy > dx)
+                {
+                    int x = _x0;
+                    for (int y = _y0; y <= _y1; y++)
+                    {
+                        image.SetPixel(x, y, color);
+                        error += 1/k;
+                        
+                        if (error >= 1)
+                        {
+
+                            x++;
+                            error -= 1;
+                        }
+                    }
+                }
+
 
             }
 
-            while(_running){
+
+
+
+            int time = 1;
+            int a = 1;
+
+            while (_running){
                 //Render image 
                 image.FillColor(Color32.Black);
                 //Process window messages
                 Application.DoEvents();
 
 
-                //example draw horizontal line
-                for (int x = 0; x < 100; ++x) {
-                    image.SetPixel(x + 100, 100, Color32.Green);
-                }
+                BresenhamLineOneQuadrant(400, 500, 400, 1000, Color32.Red);
+                time++;
+                if (time % 2 == 0) a++;
 
 
-                BresenhamLineOneQuadrant(250, 250, 750, 500, Color32.Red);
+
+                //for (int x = 0; x < form.Width; x++){
+                //    image.SetPixel(x, form.Height / 2, Color32.Red);
+                //        }
+                //for (int y = 0; y < form.Height; y++){
+                //    image.SetPixel(form.Width / 2, y, Color32.Red);
+                //}
 
                 //Show image in window
                 renderer.Present();
